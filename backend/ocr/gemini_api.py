c736f0e291806e053ca06c0891b43858d981d252
Santiago Vargas <santiagovargas@Santiagos-MacBook-Air.local>
2025-08-07 15:35:22 +0800

    new feat: added api env var, api key and got llm response

--- backend/ocr/gemini_api.py
+++ backend/ocr/gemini_api.py
@@ -1,3 +1,13 @@
-#contains LLM and gemini API integreation
+from google import genai
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
+)
+print(response.text)#contains LLM and gemini API integreation
+
+
 
 
