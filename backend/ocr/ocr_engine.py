eec0f5d21469d272e438108bc3e9a9b66315543c
Santiago-vgs <santiagovav5@gmail.com>
2025-09-03 14:51:55 +0800

    made new engine with easu ocr instead of tesseract

--- backend/ocr/ocr_engine.py
+++ backend/ocr/ocr_engine.py
@@ -1,37 +1,10 @@
-import pytesseract
-import cv2
+import easyocr
 
-from preprocessing import binarisation
-from preprocessing import noise_removal
-from preprocessing import upscaling
-from preprocessing import sharpness
+reader = easyocr.Reader(['en'])
 
+img_path = '/Users/santiagovargas/Desktop/Vision2Summary/backend/ocr/test2.jpeg'
+result = reader.readtext(img_path)
 
-image_path = "/Users/santiagovargas/Desktop/Vision2Summary/backend/ocr/test2.jpeg"
-
-# Load the image with OpenCV
-image = cv2.imread(image_path)
-
-# Add a check to ensure the image loaded successfully
-if image is None:
-    print(f"Error: Could not load image from {image_path}. Please check the path.")
-else:
-    # 1. Apply binarisation. This function should return a grayscale NumPy array.
-    # It also saves 'grayscale_test.jpeg' internally.
-    gray_image = binarisation(image)
-
-   ##### extracted_text = pytesseract.image_to_string(final_processed_image)
-
-   ### TESTING UP SCLAING #####
-    upscaled_image = upscaling(gray_image)
-
-    ####### TESTING SHARPNESS #######
-    final_processed_image = sharpness(upscaled_image)
-
-
-   ##### OUTPUT ########
-    extracted_text = pytesseract.image_to_string(final_processed_image)
-
-    print("--- Extracted Text ---") 
-    print(extracted_text)
-    
+print("\n--- Detected Text ---")
+for (bbox, text, prob) in result:
+    print(text)
